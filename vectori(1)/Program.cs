7103bb7167381e7bb384408f23444cf847355b23
Daniel Leon Andrei <daniel.andrei@emmanuel.ro>
2021-11-25 16:47:53 +0200

    Hello

--- vectori(1)/Program.cs
+++ vectori(1)/Program.cs
@@ -6,7 +6,31 @@ namespace vectori_1_
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] v = new int[100];
+            int n, s=0, nrp=0;
+            int minn = int.MaxValue;
+            int maxx = int.MinValue;
+            double med;
+
+            n = Int32.Parse(Console.ReadLine());
+            for (int i = 1; i <= n; i++)
+            {
+                v[i] = Int32.Parse(Console.ReadLine());
+                s = s + v[i];
+                if (v[i] % 2 == 0) nrp++;
+                if (v[i] < minn) minn = v[i];
+                if (v[i] > maxx) maxx = v[i];
+            }
+            double sum = Convert.ToDouble(s);
+            double nr = Convert.ToDouble(n);
+            med = sum / nr;
+
+            Console.WriteLine("Suma este: "+s);
+            Console.WriteLine("Media este: "+med);
+            Console.WriteLine("Numarul valorilor pare este: "+nrp);
+            Console.WriteLine("Minimul: "+minn);
+            Console.WriteLine("Maximul: "+maxx);
+
         }
     }
 }
